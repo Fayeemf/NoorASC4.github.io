bbb1f32ad199b08888f00b8748d6fb1d844fb0b0
NoorASC4 <rajn@bxscience.edu>
2017-08-08 23:40:28 -0400

    August 8th

---
+++ canvas/script.js
@@ -0,0 +1,101 @@
+
+
+//this entire thing does the cursor part of my website!
+    var al = [];
+    var bg;
+    function setup() {
+        bg = loadImage("background.jpg");
+        createCanvas( windowWidth, windowHeight );
+    }
+
+    function draw() {
+    background(bg);
+    
+    
+    strokeWeight( 1 ); // Restore strokeWeight 
+    
+    /*
+    * Array to store reference to each Rays() object 
+    */
+    al.push( new Rays() );
+    
+    for( var i = 0; i < al.length; i++ ) {
+        var r = al[i];
+        r.applyForce( new p5.Vector( random( -0.5, 0.5 ), random( 0.01, 0.05 ) ) );
+        r.update();
+        r.render();
+        if( r.isDead() )
+        al.shift();
+    }
+    }
+
+    function windowResized() {
+    resizeCanvas( windowWidth, windowHeight );
+    }
+
+    function Rays() {
+    this.counter = 0;
+    this.position = new p5.Vector( mouseX, mouseY );
+    this.velocity = new p5.Vector( 0, 0 );
+    this.acceleration = new p5.Vector( 0, 0 );
+    this.lifeSpan = 1;
+    }
+
+    /*
+    * Takes p5.Vector object as the initial force 
+    * This force provides the required acceleration
+    */
+    Rays.prototype.applyForce = function( force ) {
+    acceleration = force;
+    }
+
+    /*
+    * Calculates and updates
+    *
+    * 1. Velocity
+    * 2. Position
+    */
+    Rays.prototype.update = function() {
+    this.velocity.add( this.acceleration );
+    this.position.add( this.velocity );
+    this.lifeSpan -= 0.04;
+    }
+
+    /*
+    * Displays line on the document
+    */
+    Rays.prototype.render = function() {
+    var c = color( 'rgba(49, 67, 96, ' + this.lifeSpan + ')' );
+    stroke( c ); 
+    line( this.position.x, this.position.y, pmouseX, pmouseY );
+    }
+
+    /*
+    * Helps in determining whether the shift()
+    * method should be called or not
+    *
+    * [This method is important, if not used the array will
+    * be filled infinitely]
+    */
+    Rays.prototype.isDead = function() {
+    if( this.lifeSpan < 0.1 )
+        return true;
+    else
+        return false;
+    }
+
+
+//this is where i want to put the interactive background?? maybe
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
---
+++ plan.js
@@ -0,0 +1,26 @@
+/*
+Okay so what i want for my website is to ded have a naviagation bar at the top.
+ir will most likely have to stay on the top nad i want it to have like dropdown links becuase those look nice
+i want the background to be a neutral color. i dont want anything too colorful or too rowdy
+I def want like white with a grey neutral background ish, ill figure out the exact color later on. It feels like im going to be
+copying navid but im actually not. I also want a pic of me on the front cover
+i want to be wearing this blue shit thi! like i want apic with def this shirt and 
+i also want it paired with something greyish.
+I want for my webstie to be able to feature my projects somehow.
+you know what. i wan tht front page to be pretty blank with the "navigation bar" to be a guide on where to go.
+there will be a section called projects. when you click on it, it will show a slow animation of everything from the website disappearing andthe other shit reappering.
+this way you dont have to refresh the page or go to a diff one in order to see the diff websites. i also want to have 
+a hover function that makes the recatngle pop out just a bit more than it already does! this way you cna see it clearly
+which button you would want to click. 
+in the projects tab tho, i want to be able to feature my ratatouille, i think im going to work on the pokemon battle too
+and then put that on there too. if i ever do get ot finish playdead website, i will most def put it on there. it will be a pic of
+website, with a description, and if you click on it, it will redirect you directly to the website.
+I also want an about me section too. i want to be able to have a paragraph explaining how i am a rising web
+developer that would like to be advancing his skills! it will also explain my education background and i most def got to flex
+the bxsciiiiii gang! i want this page to have a pic of me too! i dont want it to be ugly and i want it side by side
+with the description. i want the font to be like sorta tall but skinny, just like me! i also would like it if this didnt suck k thanks!
+this website will also have more shit! i just have to think of more things!
+next time you are off of it, you should just be doing these plan.js files for all your upcoming projects and see what ind of crazy mind you 
+actually have nad how many more ideas you can produce.
+
+*/
